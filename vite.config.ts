e3bb8ee2006c4ff9f57bfb4d87a72428e9ec30f5
everton365 <evertonapk09@gmail.com>
2024-12-11 00:28:38 -0300

    vmn

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
       '/post': {
-        target: 'http://blognews360.duckdns.org',
+        target: 'https://blognews360.duckdns.org',
         changeOrigin: true,
       },
     },
